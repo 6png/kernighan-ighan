b80af5898d72fff2ae759ad555765d2cfd060570
6png <68672341+6png@users.noreply.github.com>
2026-02-28 10:23:23 -0800

    add: (01) 21

--- 01/21.c
+++ 01/21.c
@@ -5,21 +5,54 @@
 
 #define TAB_SZ  4
 
+int eqTab(int s[], int n) {
+    if (n != TAB_SZ) return -1;
+    for (int i=0; i<TAB_SZ; i++) {
+        if (s[i] != ' ') {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int printBuf(int buf[], int n) {
+    if (n > TAB_SZ) return -1;
+    for (int i=0; i<n; i++) {
+        putchar(buf[i]);
+    }
+    return 0;
+}
+
 int main() {
     int c;
-    int len=0, cCt=0;
+    int lineLen=0, bufCt=0;
     int buf[TAB_SZ] = {0};
 
     while ((c = getchar()) != EOF) {
-        if (cCt<TAB_SZ) {
-            ++len;  ++cCt;
-            buf[cCt] = c;
+        if (c == '\n') {
+            printBuf(buf, bufCt);
+            putchar('\n');
+            lineLen=0;  bufCt=0;
         } else {
-
-        }
+            if (bufCt<TAB_SZ) {
+                buf[bufCt] = c;
+            } else {
+                if (eqTab(buf, TAB_SZ) == 1) {
+                    putchar('\t');
+                } else {
+                    printBuf(buf, TAB_SZ);
+                }
+                bufCt=0;
+                lineLen -= TAB_SZ;
+                buf[bufCt] = c;
+            }
+            ++lineLen;  ++bufCt;
+        } 
     }
-}
 
-int eqTab(int s[]) {
-    if ()
+    printBuf(buf, bufCt);
+
+    if (lineLen > 0) {
+        putchar('\n');
+    }
 }
