8b4fe9ee4229daac840e9040c1a72c31afc20594
6png <68672341+6png@users.noreply.github.com>
2026-03-15 15:00:24 -0700

    add: (03) 03

--- 03/03.c
+++ 03/03.c
@@ -17,36 +17,33 @@ int isDigit(char c) {
     return c >= '0' && c <= '9';
 }
 
-int matches(char a, char b) {
-    return (isLower(a) && isLower(b) ||
+int validmatch(char a, char b) {
+    return ((isLower(a) && isLower(b) ||
             isUpper(a) && isUpper(b) ||
-            isDigit(a) && isDigit(b));
+            isDigit(a) && isDigit(b))
+            && a < b);
 }
 
-void expand(char s1[], int n, char s2[]) {
-    if (n<3) {
-        for (int i=0; i<n; i++) [
-            s2[i] = s1[i];
-        ]
-        return 0;
-    }
-
-    int j=0;
-    char preva=s1[0], prevb=s1[1], curr=s1[2];
-    for (int i=3; i<n; i++) {
-        preva=s1[i-2];
-        prevb=s1[i-1];
-        curr=s1[i];
-        if (prevb == '-') {
-            if (matches(preva, curr)) {
-                for (int k=preva; k <= curr; k++) {
-
-                }
+void expand(char s1[], char s2[]) {
+    int i=0, j=0;
+    while (s1[i] != '\0') {
+        if (s1[i+1] == '-'
+            && s1[i+2] != '\0'
+            && validmatch(s1[i], s1[i+2])) {
+            for (char k=s1[i]; k<s1[i+2]; k++) {
+                s2[j++] = k;
             }
+            i+=2;
+        } else {
+            s2[j++] = s1[i++];
         }
     }
+    s2[j] = '\0';
 }
 
 int main() {
-
+    char s1[] = "hello a-b a-b-c a-e-z 0-9 ab-c -a a-d dcba";
+    char s2[100000];
+    expand(s1, s2);
+    printf("%s\n%s\n", s1, s2);
 }
